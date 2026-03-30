dfc12916657023ca18fae9cfcef4d63dc2768421
songence <aniviason12@gmail.com>
2026-03-30 23:09:48 +0700

    Update source code

---
+++ python/toiuutainguyen.py
@@ -0,0 +1,9 @@
+luongthuc = 0
+go = 150
+dungkhi = 0
+vaskasia = str(100) + " go"
+tainguyencan = str(luongthuc) + " luongthuc, " + str(go) + " go, " + str(dungkhi) + " dungkhi "
+tainguyendu = vaskasia
+print(vaskasia)
+print(tainguyencan)
+print(tainguyendu)
